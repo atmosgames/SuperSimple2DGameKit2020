64c7db6eb89f54907febea79ebb72a83881d9fde
atmosgames <atmosgames@gmail.com>
2020-07-31 15:46:04 -0400

    18 Floating Platforms 7312020

---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : PhysicsObject
+{
+    [SerializeField] private float maxSpeed = 1;
+    [SerializeField] private float jumpPower = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = new Vector2(Input.GetAxis("Horizontal")*maxSpeed, 0);
+
+        //If the player presses "Jump" and we're grounded, set the velocity to a jump power value
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpPower;
+        }
+    }
+}
---
+++ Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Allows the controller to move each layer based on the parallaxAmount!*/
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [Range(-1f, 1f)]
+    public float parallaxAmount; //The amount of parallax! 1 simulates being close to the camera, -1 simulates being very far from the camera!
+    [System.NonSerialized] public Vector3 newPosition;
+    private bool adjusted = false;
+
+    public void MoveLayer(float positionChangeX, float positionChangeY)
+    {   
+        newPosition = transform.localPosition;
+        newPosition.x -= positionChangeX * (-parallaxAmount * 40) * (Time.deltaTime);
+        newPosition.y -= positionChangeY * (-parallaxAmount * 40) * (Time.deltaTime);
+        transform.localPosition = newPosition;
+    }
+
+}
+ 
