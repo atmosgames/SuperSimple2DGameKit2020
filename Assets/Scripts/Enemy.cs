5ef6dd0e170b7a7a84dfcadac2be1d0bf0a61102
atmosgames <atmosgames@gmail.com>
2020-08-06 12:20:26 -0400

    Enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : PhysicsObject
+{
+    [SerializeField] private float maxSpeed;
+    private int direction = 1;
+    private RaycastHit2D rightLedgeRaycastHit;
+    private RaycastHit2D leftLedgeRaycastHit;
+    private RaycastHit2D rightWallRaycastHit;
+    private RaycastHit2D leftWallRaycastHit;
+    [SerializeField] private LayerMask rayCastLayerMask;
+
+    [SerializeField] private Vector2 rayCastOffset;
+    [SerializeField] private float rayCastLength = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = new Vector2(maxSpeed*direction, 0);
+
+        //Check for right ledge!
+        rightLedgeRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down, rayCastLength);
+        Debug.DrawRay(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down * rayCastLength, Color.blue);
+        if(rightLedgeRaycastHit.collider == null) direction = -1;
+
+        //Check for left ledge!
+        leftLedgeRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down, rayCastLength);
+        Debug.DrawRay(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y + rayCastOffset.y), Vector2.down * rayCastLength, Color.green);
+        if (leftLedgeRaycastHit.collider == null) direction = 1;
+
+        //Check for right wall!
+        rightWallRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, rayCastLength, rayCastLayerMask);
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), Vector2.right * rayCastLength, Color.red);
+        if (rightWallRaycastHit.collider != null) direction = -1;
+
+        //Check for left wall!
+        leftWallRaycastHit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.left, rayCastLength, rayCastLayerMask);
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y), Vector2.left * rayCastLength, Color.magenta);
+        if (leftWallRaycastHit.collider != null) direction = 1;
+    }
+}
